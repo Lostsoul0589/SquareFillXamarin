4b9c78599bbb2d5608a698865f74154d81166c58
Luce Carter <luce@lucyscarter.co.uk>
2017-04-03 22:00:12 +0100

    Initial Commit + Started ShapeSetBuilder class

---
+++ SquareFillXamarinUnitTests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace SquareFillXamarinUnitTests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test]
+		public void CheckBasicFunctionality()
+		{
+			Assert.AreEqual(true, true);
+		}
+	}
+}
