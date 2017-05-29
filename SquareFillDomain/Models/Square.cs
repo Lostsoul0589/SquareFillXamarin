161cfcdad53565ef8c64744d2235d09d2e624b64
claresudbery <clareworking@claresudbery.co.uk>
2017-05-29 19:53:30 +0100

    Removed all references to shape centre from Square class (apart from constructor)

--- SquareFillDomain/Models/Square.cs
+++ SquareFillDomain/Models/Square.cs
@@ -6,7 +6,6 @@ namespace SquareFillDomain.Models
 {
     public class Square
     {
-        public SquareFillPoint PositionRelativeToParent { get; set; }
         public SquareFillPoint PositionRelativeToParentCorner { get; set; }
         public ISquareView Sprite { get; private set; }
         public SquareFillPoint TopLeftCorner { get; private set; }
@@ -14,35 +13,19 @@ namespace SquareFillDomain.Models
         public Square()
         {
             TopLeftCorner = new SquareFillPoint(x: 0, y: 0);
-            PositionRelativeToParent = new SquareFillPoint(x: 0, y: 0);
             PositionRelativeToParentCorner = new SquareFillPoint(x: 0, y: 0);
         }
 
         public Square(SquareFillPoint positionRelativeToParent, SquareFillPoint positionRelativeToParentCorner, ISquareView sprite)
         {
-            PositionRelativeToParent = positionRelativeToParent;
             PositionRelativeToParentCorner = positionRelativeToParentCorner;
             Sprite = sprite;
             TopLeftCorner = new SquareFillPoint(x: 0, y: 0);
         }
 
-        public void CalculateOrigin(SquareFillPoint parentShapeCentre)
-        {
-            TopLeftCorner = CalculatePotentialOrigin(parentShapeCentre: parentShapeCentre);
-        }
-
         public void CalculateTopLeftCorner(SquareFillPoint parentTopLeftCorner)
         {
             TopLeftCorner = CalculatePotentialTopLeftCorner(parentTopLeftCorner: parentTopLeftCorner);
-        }
-
-        public SquareFillPoint CalculatePotentialOrigin(SquareFillPoint parentShapeCentre)
-        {
-            return new SquareFillPoint(
-                x: parentShapeCentre.X + (PositionRelativeToParent.X * ShapeConstants.SquareWidth)
-                    - ShapeConstants.SquareWidth / 2,
-                y: parentShapeCentre.Y + (PositionRelativeToParent.Y * ShapeConstants.SquareWidth)
-                    - ShapeConstants.SquareWidth / 2);
         }
 
         public SquareFillPoint CalculatePotentialTopLeftCorner(SquareFillPoint parentTopLeftCorner)
